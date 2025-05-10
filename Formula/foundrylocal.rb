e21b4a166f834829bac503005a01d6391cec16a6
Nat Kershaw (MSFT) <nakersha@microsoft.com>
2025-05-10 16:32:22 -0700

    Bump foundrylocal 0.2.9261.3846 (#6)

--- Formula/foundrylocal.rb
+++ Formula/foundrylocal.rb
@@ -1,18 +1,16 @@
-require "formula"
-require_relative "../custom_download_strategy.rb"
+require_relative "custom_download_strategy"
 
 class Foundrylocal < Formula
-  desc "Foundry Local CLI and Inference Agent installer"
+  desc "Formula for Foundry Local CLI and Inference Agent installer"
   homepage "https://github.com/microsoft/homebrew-foundrylocal"
-  url "https://github.com/microsoft/homebrew-foundrylocal/releases/download/v0.2.9257.39116/FoundryLocal-osx-arm64-0.2.9257.39116.zip", :using => GitHubPrivateRepositoryReleaseDownloadStrategy
-  sha256 "f852c9c4eecadd7fae102c5fe27c7ed837aa6895a4475e4c8a2305ab7d423b22"
-  version "0.2.9257.39116"
+  url "https://github.com/microsoft/homebrew-foundrylocal/releases/download/v0.2.9261.3846/FoundryLocal-osx-arm64-0.2.9261.3846.zip", using: GitHubPrivateRepositoryReleaseDownloadStrategy
+  sha256 "062a9afc78a827b814551dc1ecc8415e6529b3f79d88e6331d6053d42fa087a3"
 
   def install
     bin.install "bin/foundry"
-    bin.install "bin/libonnxruntime-genai.dylib"
-    bin.install "bin/libonnxruntime.dylib"
+    lib.install "bin/libonnxruntime-genai.dylib"
+    lib.install "bin/libonnxruntime.dylib"
     bin.install "bin/Inference.Service.Agent"
-    bin.install "bin/onnx.yml"
+    lib.install "bin/onnx.yml"
   end
 end
