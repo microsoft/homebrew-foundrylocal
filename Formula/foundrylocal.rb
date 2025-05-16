a405f0384311bf723064b604259694b4dd691983
Nat Kershaw (MSFT) <nakersha@microsoft.com>
2025-05-16 15:54:46 -0700

    Update RC to 0.2.9265 (#9)
    Co-authored-by: Nat Kershaw <nakershaw@microsoft.com>

--- Formula/foundrylocal.rb
+++ Formula/foundrylocal.rb
@@ -3,14 +3,13 @@ require_relative "custom_download_strategy"
 class Foundrylocal < Formula
   desc "Formula for Foundry Local CLI and Inference Agent installer"
   homepage "https://github.com/microsoft/homebrew-foundrylocal"
-  url "https://github.com/microsoft/Foundry-Local/releases/download/v0.2.9261/FoundryLocal-osx-arm64-0.2.9263.31084.zip", using: GitHubPrivateRepositoryReleaseDownloadStrategy
-  sha256 "3a6b1dc41e7be5cb8f478b7b23e4e79c353a1ce4717b15aa7788f5afd8e82451"
+  url "https://github.com/microsoft/Foundry-Local/releases/download/v0.2.9265/FoundryLocal-osx-arm64-0.2.9265.18890.zip",  using: GitHubPrivateRepositoryReleaseDownloadStrategy
+  sha256 "b76c0ce4284c8be23d4d0af9af3121e8bbbbd77e474b0e3bd3f434bdb9f818bc"
 
   def install
     bin.install "bin/foundry"
     bin.install "bin/libonnxruntime-genai.dylib"
     bin.install "bin/libonnxruntime.dylib"
     bin.install "bin/Inference.Service.Agent"
-    bin.install "bin/onnx.yml"
   end
 end
