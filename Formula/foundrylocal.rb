8303ba55756a9f19f76d7638a08299bc004bf55f
Baiju Meswani <baijumeswani@gmail.com>
2025-05-19 13:49:13 -0700

    Install symlink to only the foundry executable on MacOS (#14)

--- Formula/foundrylocal.rb
+++ Formula/foundrylocal.rb
@@ -4,14 +4,13 @@ class Foundrylocal < Formula
   url "https://github.com/microsoft/Foundry-Local/releases/download/v0.3.9267/FoundryLocal-osx-arm64-0.3.9267.42993.zip"
   sha256 "95b1747a7e89e8ec690b3fcb6a46bdd4c2fd07faa3c28fcdc98ee842a32f9b8b"
 
-
   def install
-    bin.install "bin/foundry"
-    bin.install "bin/Inference.Service.Agent"
+    prefix.install Dir["bin/foundry"]
+    prefix.install Dir["bin/Inference.Service.Agent"]
+    prefix.install Dir["bin/libonnxruntime-genai.dylib"]
+    prefix.install Dir["bin/libonnxruntime.dylib"]
+    prefix.install Dir["bin/appsettings.json"]
 
-    bin.install "bin/libonnxruntime-genai.dylib"
-    bin.install "bin/libonnxruntime.dylib"
-    
-    bin.install "bin/appsettings.json"
+    bin.install_symlink prefix/"foundry"
   end
 end
