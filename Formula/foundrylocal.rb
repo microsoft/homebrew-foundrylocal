7a22f9448e4815728a0631c8b237662ac0ee2d23
Nat Kershaw (MSFT) <nakersha@microsoft.com>
2025-05-18 17:45:38 -0700

    Update to public preview release (#10)

--- Formula/foundrylocal.rb
+++ Formula/foundrylocal.rb
@@ -1,9 +1,7 @@
-require_relative "custom_download_strategy"
-
 class Foundrylocal < Formula
   desc "Formula for Foundry Local CLI and Inference Agent installer"
-  homepage "https://github.com/microsoft/homebrew-foundrylocal"
-  url "https://github.com/microsoft/Foundry-Local/releases/download/v0.3.9267/FoundryLocal-osx-arm64-0.3.9267.42993.zip",  using: GitHubPrivateRepositoryReleaseDownloadStrategy
+  homepage "https://github.com/microsoft/Foundry-Local"
+  url "https://github.com/microsoft/Foundry-Local/releases/download/v0.3.9267/FoundryLocal-osx-arm64-0.3.9267.42993.zip"
   sha256 "95b1747a7e89e8ec690b3fcb6a46bdd4c2fd07faa3c28fcdc98ee842a32f9b8b"
 
   def install
