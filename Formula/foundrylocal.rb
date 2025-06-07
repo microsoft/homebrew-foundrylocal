ce047657a87741ab5cf1a74248a99f3455f169d6
Baiju Meswani <baijumeswani@gmail.com>
2025-06-06 18:12:17 -0700

    Revert "Update foundry-local version (#15)" (#16)

--- Formula/foundrylocal.rb
+++ Formula/foundrylocal.rb
@@ -1,8 +1,8 @@
 class Foundrylocal < Formula
   desc "Formula for Foundry Local CLI and Inference Agent installer"
   homepage "https://github.com/microsoft/Foundry-Local"
-  url "https://github.com/microsoft/Foundry-Local/releases/download/v0.4.91/FoundryLocal-osx-arm64-0.4.91.zip"
-  sha256 "beebf8b1822a6529841c99ba3d089ffd9f8b155b2e5bcf6b5b69af8aea86ac7d"
+  url "https://github.com/microsoft/Foundry-Local/releases/download/v0.3.9267/FoundryLocal-osx-arm64-0.3.9267.42993.zip"
+  sha256 "95b1747a7e89e8ec690b3fcb6a46bdd4c2fd07faa3c28fcdc98ee842a32f9b8b"
 
   def install
     prefix.install Dir["bin/foundry"]
