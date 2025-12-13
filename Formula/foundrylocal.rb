250c0114b5190519243be8ca72532e37a8bf4d5b
Nat Kershaw (MSFT) <nakersha@microsoft.com>
2025-12-13 01:05:18 +0000

    Update to 0.8.115 (#27)

--- Formula/foundrylocal.rb
+++ Formula/foundrylocal.rb
@@ -1,8 +1,8 @@
 class Foundrylocal < Formula
   desc "Formula for Foundry Local CLI and Inference Agent installer"
   homepage "https://github.com/microsoft/Foundry-Local"
-  url "https://github.com/microsoft/Foundry-Local/releases/download/v0.8.113/FoundryLocal-osx-arm64-0.8.113.zip"
-  sha256 "5c37a68d97b7d9c0fe8b0ce76d3572494dc31d3977399323e55ec546f40663aa"
+  url "https://github.com/microsoft/Foundry-Local/releases/download/v0.8.115/FoundryLocal-osx-arm64-0.8.115.zip"
+  sha256 "2b3dbdd17b6336ffc70cd15dc3cbbe4aff7835fb8b281b7816cb80444b14f202"
   license "https://github.com/microsoft/Foundry-Local/blob/main/LICENSE"
 
   def install
